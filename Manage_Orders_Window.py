99f17360f8540fa66f2f0fc6ebecda05431247ac
Mohammed Mahmoud <myworkmoud@gmail.com>
2024-12-22 03:14:28 +0200

    make functionality in button add to cart and remove cart and clear cart

--- Manage_Orders_Window.py
+++ Manage_Orders_Window.py
@@ -1,10 +1,18 @@
 from tkinter import *
+
+from PyQt5.sip import delete
+
+from book import Book
+from manage_books import *
+from Validation import Validation
 from tkinter import ttk, messagebox
 
 
 class ManageOrders:
     def __init__(self, master, show_home):
         self.master = master
+        self.orders = []
+        self.manage_books = ManageBook(SQLite("bookstore.db"))
         self.show_home = show_home
         self.frame = Frame(master)
         self.orders = []
@@ -38,15 +46,15 @@ class ManageOrders:
         )
         self.quantity_entry.grid(row=2, column=1, padx=5, pady=5)
 
-        btn1 = Button(form_frame, text="Add To Cart", command=self.add_order, bg="gray")
+        btn1 = Button(form_frame, text="Add To Cart", command=self.add_to_cart, bg="gray")
         btn1.grid(row=3, column=0, pady=10)
 
         btn2 = Button(
-            form_frame, text="Add Remove To Cart", command=self.add_order, bg="gray"
+            form_frame, text="Remove To Cart", command=self.remove_from_cart, bg="gray"
         )
         btn2.grid(row=3, column=1, pady=10)
 
-        btn3 = Button(form_frame, text="Clear cart", command=self.add_order, bg="gray")
+        btn3 = Button(form_frame, text="Clear cart", command=self.clear_cart, bg="gray")
         btn3.grid(row=3, column=2, pady=10)
 
         btn4 = Button(form_frame, text="Git Bill", command=self.add_order, bg="gray")
@@ -57,7 +65,7 @@ class ManageOrders:
 
         # --------------------------------------
 
-        # -------------------Orders Table-------------------
+        # -------------------Books Table-------------------
         self.books_tree = ttk.Treeview(
             self.frame,
             columns=("book_id", "title", "author", "price"),
@@ -69,25 +77,115 @@ class ManageOrders:
         self.books_tree.heading("price", text="Price")
         self.books_tree.pack(pady=10)
         self.books_tree.pack(pady=10)
+        self.load_books()
 
-        # -------------------Books Table-------------------
+        # -------------------Orders Table-------------------
         self.tree = ttk.Treeview(
             self.frame,
-            columns=("id", "book_id", "user_id", "quantity"),
+            columns=("book_id", "title", "author", "price", "quantity"),
             show="headings",
         )
-        self.tree.heading("id", text="ID")
+
         self.tree.heading("book_id", text="Book ID")
-        self.tree.heading("user_id", text="User ID")
+        self.tree.heading("title", text="Title")
+        self.tree.heading("author", text="Author")
+        self.tree.heading("price", text="Price")
         self.tree.heading("quantity", text="Quantity")
+
         self.tree.pack(pady=10)
-     
-     
+        self.load_orders()
+
+    def add_to_cart(self):
+        selected_item = self.books_tree.selection()
+        if selected_item:
+            item = self.books_tree.item(selected_item)
+            book_id = item["values"][0]
+            book = self.manage_books.get_book(book_id)
+            quantity = int(self.quantity_entry.get())
+
+            if quantity <= book.get_quantity():
+                book.set_quantity(book.get_quantity() - quantity)
+                self.manage_books.update_book(book)
+                book.set_quantity(quantity)
+
+                self.orders.append(book)
+
+                self.load_books()
+                self.load_orders()
+            else:
+                messagebox.showerror("Error", "the book is limited")
+        else:
+            messagebox.showerror("Error", "Select a book")
+
+    def remove_from_cart(self):
+        selected_item = self.tree.selection()
+        if selected_item:
+            item = self.tree.item(selected_item)
+            book_id = item["values"][0]
+            book = self.manage_books.get_book(book_id)
+            quantity = int(self.quantity_entry.get())
+
+            for item in self.orders:
+                if item.get_book_id() == book_id:
+                    self.orders.remove(item)
+
+            book.set_quantity(book.get_quantity() + quantity)
+            self.manage_books.update_book(book)
+
+            self.load_books()
+            self.load_orders()
+        else:
+            messagebox.showerror("Error", "Select a book")
+
+    def clear_cart(self):
+        quantity = int(self.quantity_entry.get())
+
+        for item in self.orders:
+            book_id = item.get_book_id()
+            book = self.manage_books.get_book(book_id)
+            book.set_quantity(book.get_quantity() + item.get_quantity())
+            self.manage_books.update_book(book)
+
+        self.orders.clear()
+        self.load_books()
+        self.load_orders()
+
+    def load_books(self):
+        self.reset_book_table()
+        self.books = self.manage_books.get_all_books()
+        # print(self.books)
+        for row in self.books:
+            self.books_tree.insert("", END, values=(row.get_book_id(),
+                                                    row.get_title(),
+                                                    row.get_author(),
+                                                    row.get_price(),
+                                                    row.get_quantity()))
+
+    def reset_book_table(self):
+        for item in self.books_tree.get_children():
+            self.books_tree.delete(item)
+
     def load_orders(self):
-        for row in self.tree.get_children():
-            self.tree.delete(row)
-        for idx, order in enumerate(self.orders):
-            self.tree.insert("", "end", values=(idx + 1, *order))
+        self.reset_order_table()
+        for row in self.orders:
+            self.tree.insert("", END, values=(row.get_book_id(),
+                                                    row.get_title(),
+                                                    row.get_author(),
+                                                    row.get_price(),
+                                                    row.get_quantity()))
+        # self.books = self.manage_books.get_all_books()
+        # # print(self.books)
+
+        # for row in self.books:
+        #     self.tree.insert("", END, values=(row.get_book_id(),
+        #                                       row.get_title(),
+        #                                       row.get_author(),
+        #                                       row.get_price(),
+        #                                       row.get_quantity()))
+
+    def reset_order_table(self):
+        for item in self.tree.get_children():
+            self.tree.delete(item)
 
     def add_order(self):
         book_id = self.book_id_entry.get()
@@ -108,5 +206,6 @@ class ManageOrders:
         self.frame.pack_forget()
 
     def back_to_home(self):
+        self.clear_cart()
         self.hide()
         self.show_home()
